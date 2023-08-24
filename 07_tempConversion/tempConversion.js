dc49146b64cff7770a2d10bde5b78a5ffedff44d
muhammadfiqiha <muhammadfiqih910@gmail.com>
2023-08-24 14:17:00 +0700

    finish exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  
+  if (typeof(tempInFahrenheit) !== 'number') return 'ERROR';
+  
+  return Math.round((tempInFahrenheit - 32) * 5/9 * 10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+
+  if (typeof(tempInCelsius) !== 'number') return 'ERROR';
+
+  return Math.round(((tempInCelsius * 9/5) + 32) * 10)/10;
 };
 
 // Do not edit below this line
